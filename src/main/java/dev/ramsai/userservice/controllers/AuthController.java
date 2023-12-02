96847e738e228a9eace9885eb64c84bec3593106
Ramsaidhanumuri <ramsaidhanumuri@gmail.com>
2023-12-02 17:22:49 +0530

    login post mapping method added

--- src/main/java/dev/ramsai/userservice/controllers/AuthController.java
+++ src/main/java/dev/ramsai/userservice/controllers/AuthController.java
@@ -1,10 +1,26 @@
 package dev.ramsai.userservice.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.ramsai.userservice.dtos.LoginRequestDto;
+import dev.ramsai.userservice.dtos.LogoutRequestDto;
+import dev.ramsai.userservice.dtos.UserDto;
+import dev.ramsai.userservice.services.AuthService;
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/auth")
+@AllArgsConstructor
 public class AuthController {
 
+	private AuthService authService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<UserDto> login(@RequestBody LoginRequestDto request){
+		return authService.login(request.getEmail(), request.getPassword());
+	}
 }
---
+++ src/main/java/dev/ramsai/userservice/dtos/LoginRequestDto.java
@@ -0,0 +1,12 @@
+package dev.ramsai.userservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequestDto {
+
+	private String email;
+    private String password;
+}
