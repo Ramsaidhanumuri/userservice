de77499bcc1a74d472f531c816c340f1705cc1be
Ramsaidhanumuri <ramsaidhanumuri@gmail.com>
2023-12-02 19:30:55 +0530

    BCryptPasswordEncoder bean and SpringSecurity class created

--- src/main/java/dev/ramsai/userservice/repositories/UserRepository.java
+++ src/main/java/dev/ramsai/userservice/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package dev.ramsai.userservice.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.ramsai.userservice.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
