9cc9df5d206b86cf707920a63a46940d30b66d39
Ramsaidhanumuri <ramsaidhanumuri@gmail.com>
2023-12-02 22:39:34 +0530

    csrf disabled in SpringSecurity class

--- src/main/java/dev/ramsai/userservice/security/SpringSecurity.java
+++ src/main/java/dev/ramsai/userservice/security/SpringSecurity.java
@@ -2,11 +2,19 @@ package dev.ramsai.userservice.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurity {
 
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		 http.csrf().disable();
+	    return http.build();
+	}
+	
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
