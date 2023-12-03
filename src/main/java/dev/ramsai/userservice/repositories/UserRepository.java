0e4adff22c818c8a3dd3e085a83691aeda24a600
Ramsaidhanumuri <ramsaidhanumuri@gmail.com>
2023-12-03 12:36:30 +0530

    findbyemail method added in user repository

--- src/main/java/dev/ramsai/userservice/repositories/UserRepository.java
+++ src/main/java/dev/ramsai/userservice/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package dev.ramsai.userservice.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.ramsai.userservice.models.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Optional<User> findByEmail(String email);
 }
